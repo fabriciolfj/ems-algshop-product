ad45725b670d758a44d5266637f4a29229573ffc
fabricio <fabricio.jacob@outlook.com>
2026-09-10 21:42:52 -0300

    feat: add

--- src/main/java/com/algaworks/algashop/product/catalog/infrastructure/utility/mapper/ModelMapperConfig.java
+++ src/main/java/com/algaworks/algashop/product/catalog/infrastructure/utility/mapper/ModelMapperConfig.java
@@ -42,16 +42,6 @@ public class ModelMapperConfig {
                                     .map(Product::getName, ProductDetailOutput::setSlug);
                         }
                 );
-
-        modelMapper.createTypeMap(Product.class, ProductSummaryOutput.class)
-                .addMappings(
-                        mapping -> {
-                            mapping.using(fromStringToSlugConverter)
-                                    .map(Product::getName, ProductSummaryOutput::setSlug);
-                            mapping.using(fromStringtoShortStringConverter)
-                                    .map(Product::getDescription, ProductSummaryOutput::setShortDescription);
-                        }
-                );
     }
 
 }
